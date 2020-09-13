53cc19499b782786af33f208ab075da9d9ce452b
Carlos Santiago <5726971+csantiago132@users.noreply.github.com>
2020-09-13 13:24:41 -0500

    Persisting Data

--- src/main/java/com/example/mvc_basics/controller/HomeController.java
+++ src/main/java/com/example/mvc_basics/controller/HomeController.java
@@ -1,18 +1,34 @@
 package com.example.mvc_basics.controller;
 
+import com.example.mvc_basics.MessageForm;
+import com.example.mvc_basics.MessageListService.MessageListService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 
-import java.time.Instant;
 
 @Controller
 public class HomeController {
 
-    @RequestMapping("/home")
-    public String getHomePage(Model model) {
-        model.addAttribute("greetings", new String[]{"hi", "hello", "good-bye"});
+    private MessageListService messageListService;
+
+    public HomeController(MessageListService messageListService) {
+        this.messageListService = messageListService;
+    }
+
+    @GetMapping("/home")
+    public String getHomePage(@ModelAttribute("newMessage") MessageForm newMessage, Model model) {
+        model.addAttribute("greetings", this.messageListService.getMessages());
         return "home";
     }
 
+    @PostMapping("/home")
+    public String addMessage(@ModelAttribute("newMessage") MessageForm messageForm, Model model){
+        messageListService.addMessage(messageForm.getText());
+        model.addAttribute("greetings" , messageListService.getMessages());
+        messageForm.setText("");
+        return "home";
+    }
 }
